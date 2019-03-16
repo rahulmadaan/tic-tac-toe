89d89c092930c7fc645d71a1d83a2edb520f586e
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2019-03-16 23:36:53 +0530

    Add addPlayer method and check for player turn
    * Add method in game class to check that whether the move of player is
    valid or not.
    * Add method in game to get its gamekey.

--- src/game.js
+++ src/game.js
@@ -11,16 +11,27 @@ const board = {
 };
 
 class Game {
-  constructor(gameKey, hostPlayer) {
+  constructor(gameKey, hostPlayer, joinedPlayer) {
     this.gameKey = gameKey;
     this.currentPlayer = hostPlayer;
     this.board = board;
+    this.joinedPlayer = "";
+  }
+  getBoard() {
+    return this.board;
   }
 
+  addPlayer(joinedPlayer) {
+    if (!this.joinedPlayer) {
+      this.joinedPlayer = joinedPlayer;
+    }
+  }
   isCurrentPlayer(playerId) {
     return this.currentPlayer.getId() == playerId;
   }
-
+  getKey() {
+    return this.gameKey;
+  }
 }
 
 module.exports = Game;
